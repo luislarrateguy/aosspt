c80822e2a4797f7600d511b7b0582f1dfbfe2ecf
luisignacio <luisignacio@e436eddc-da2e-0410-b8d8-4f9d6c72f632>
2007-03-27 19:43:07 +0000

    cambie los comentarios para que se vean bien en VIM

--- tp1/src/servidor.c
+++ tp1/src/servidor.c
@@ -22,13 +22,13 @@ void buscarRespuesta(char *,char *);
 
 int main(int argc, char *argv[]) {
 	int socket_fd, socket_nuevo_fd, tam_dir_cliente;		/* socket que escucha peticiones, socket para procesar request, tam. est direccion */
-	char *request;															/* buffer de lectura */
-	char *response;															/* buffer de escritura */
-	struct sockaddr_in dir_servidor, dir_cliente;				/*  */
-	int n,cant;																	/* cant=Cantidad de datos recibidos. */
-	struct sigaction sa;													/* Utilizado para manejar las senyales al proceso */
+	char *request;											/* buffer de lectura */
+	char *response;											/* buffer de escritura */
+	struct sockaddr_in dir_servidor, dir_cliente;			/*  */
+	int n,cant;												/* cant=Cantidad de datos recibidos. */
+	struct sigaction sa;									/* Utilizado para manejar las senyales al proceso */
 
-	sa.sa_handler = sigchld_handler;								/* handler de senyales */
+	sa.sa_handler = sigchld_handler;						/* handler de senyales */
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_RESTART;
 	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
@@ -36,7 +36,7 @@ int main(int argc, char *argv[]) {
 		exit(1);
 	}
 	
-	socket_fd = abrirSocket();											/* Creo un socket para escuchas peticiones */
+	socket_fd = abrirSocket();								/* Creo un socket para escuchas peticiones */
 		#if DEBUG
 		printf("Iniciando servidor...\n"); 
 		#endif
@@ -44,26 +44,28 @@ int main(int argc, char *argv[]) {
 		#if DEBUG
 		printf("Binding...\n"); 
 		#endif
-	ligar(socket_fd,&dir_servidor);										/* Hago un bind entre el socket, la direccion y el puerto */
+	ligar(socket_fd,&dir_servidor);							/* Hago un bind entre el socket, la direccion y el puerto */
 		#if DEBUG
 		printf("Conectado.\n"); 
 		#endif
-	escuchar(socket_fd);													/* Activo la escucha */
+	escuchar(socket_fd);									/* Activo la escucha */
 		printf("Escuchando en IP: %s Puerto: 2222\n", inet_ntoa(dir_servidor.sin_addr)); 
 
 
 	while(1) {  // Bucle infinito. Se corta con senyales.
-		int err = aceptarRequest(socket_fd, &socket_nuevo_fd, &dir_cliente);	/* Creo un socket cuando recibo una request, para leer los datos */
+		int err = aceptarRequest(socket_fd, &socket_nuevo_fd, &dir_cliente);	/* Creo un socket cuando recibo 
+																				una request, para leer los datos */
 		if (err)
 			continue;
 		
-		if (!fork()) {																								/* Este es el hijo. El socket original continua en el padre */
-			close(socket_fd);																					/* por lo tanto ciero el primer socket.*/
+		if (!fork()) {			/* Este es el hijo. El socket original continua en el padre */
+			close(socket_fd);	/* por lo tanto ciero el primer socket.*/
 				#if DEBUG			
-				printf("Aceptando request de IP: %s Puerto: %d\n", inet_ntoa(dir_cliente.sin_addr),ntohs(dir_cliente.sin_port)); 
+				printf("Aceptando request de IP: %s Puerto: %d\n", 
+						inet_ntoa(dir_cliente.sin_addr),ntohs(dir_cliente.sin_port)); 
 				#endif
-			leer(socket_nuevo_fd,request, &cant);												/* Lee los datos enviados por el cliente */
-			buscarRespuesta(request,response);												/* Busca una respuesta y arma el resultado en "response" */
+			leer(socket_nuevo_fd,request, &cant);		/* Lee los datos enviados por el cliente */
+			buscarRespuesta(request,response);			/* Busca una respuesta y arma el resultado en "response" */
 				#if DEBUG
 				printf("El cliente dice: %s\n",request);
 				#endif	
@@ -71,7 +73,7 @@ int main(int argc, char *argv[]) {
 			close(socket_nuevo_fd);
 			exit(0);
 		}
-		close(socket_nuevo_fd);  																		/* Cierro el descriptor en el padre del FORK */
+		close(socket_nuevo_fd);  						/* Cierro el descriptor en el padre del FORK */
 	}
 }
 
