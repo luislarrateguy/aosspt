48159d92595af144c389ea1fe3442f0026cf36a6
miltondp <miltondp@e436eddc-da2e-0410-b8d8-4f9d6c72f632>
2007-03-27 12:33:05 +0000

    - Agregué la licencia a la documentación, pero la imagen no se imprime con buena calidad. Después lo voy a retocar. - Subo el código simple del servidor y cliente. El de nacho estaba más completo, pero vamos a arrancar con esto asi lo entendemos.

--- tp1/src/cliente.c
+++ tp1/src/cliente.c
@@ -1,64 +1,70 @@
-    /*
-    ** cliente.c
-    */
+/*
+ * cliente.c
+ */
 
-    #include <stdio.h>
-    #include <stdlib.h>
-    #include <unistd.h>
-    #include <errno.h>
-    #include <string.h>
-    #include <netdb.h>
-    #include <sys/types.h>
-    #include <netinet/in.h>
-    #include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
 
-    #define DESTPORT 2222
-    #define DESTADDR "0.0.0.0"
+/*void error(char *msg) {
+	perror(msg);
+	exit(0);
+}*/
 
-    #define MAXDATASIZE 100 
+int main(int argc, char *argv[]) {
+	int sockfd, portno, n;
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
 
-    int main(int argc, char *argv[])
-    {
-        int fd, cant;  
-        char buf[MAXDATASIZE];
-        struct sockaddr_in dest_addr; 		// información de la dirección de destino 
+	char buffer[256];
+	if (argc < 3) {
+		fprintf(stderr,"usage %s hostname port\n", argv[0]);
+		exit(0);
+	}
 
-        if (argc != 3) {
-		//Ayuda del uso
-            fprintf(stderr,"Se usa: cliente SERVER_IP MSG\n");
-            exit(1);
-        }
+	portno = atoi(argv[2]);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
+	if (sockfd < 0) 
+		error("ERROR opening socket");
 
-        if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-            perror("socket");
-            exit(1);
-        }
+	server = gethostbyname(argv[1]);
+	if (server == NULL) {
+		fprintf(stderr,"ERROR, no such host\n");
+		exit(0);
+	}
 
-        dest_addr.sin_family = AF_INET;    // Ordenación de bytes de la máquina 
-        dest_addr.sin_port = htons(DESTPORT);  // short, Ordenación de bytes de la red 
-        dest_addr.sin_addr.s_addr = htonl(inet_addr(argv[1]));
-        memset(&(dest_addr.sin_zero),'\0', 8);  // poner a cero el resto de la estructura 
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr, 
+			(char *)&serv_addr.sin_addr.s_addr,
+			server->h_length);
 
-        if (connect(fd, (struct sockaddr *)&dest_addr,
-                                              sizeof(struct sockaddr)) == -1) {
-            perror("connect");
-            exit(1);
-        }
-	if (send(fd, argv[2], 14, 0) == -1)
-		perror("send");
+	serv_addr.sin_port = htons(portno);
 
-        if ((cant=recv(fd, buf, MAXDATASIZE-1, 0)) == -1) {
-            perror("recv");
-            exit(1);
-        }
+	if (connect(sockfd,&serv_addr,sizeof(serv_addr)) < 0) 
+		error("ERROR connecting");
 
-        buf[cant] = '\0';
+	printf("Please enter the message: ");
+	bzero(buffer,256);
+	fgets(buffer,255,stdin);
+	n = write(sockfd,buffer,strlen(buffer));
 
-	// Imprimo el mensaje que me viene del servidor
-        printf("El servidor dijo: %s",buf);
+	if (n < 0) 
+		error("ERROR writing to socket");
 
-        close(fd);
+	bzero(buffer,256);
+	n = read(sockfd,buffer,255);
 
-        return 0;
-    } 
+	if (n < 0) 
+		error("ERROR reading from socket");
+
+	printf("%s\n",buffer);
+	return 0;
+}
--- tp1/src/servidor.c
+++ tp1/src/servidor.c
@@ -1,104 +1,63 @@
 /*
-** server.c -- a stream socket server demo
-*/
+ * servidor.c
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
-#include <sys/types.h>
+#include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
-#include <sys/wait.h>
-#include <signal.h>
 
-#define PORT 2222
-#define ADDR "192.168.0.2"
+/*void error(char *msg) {
+	perror(msg);
+	exit(1);
+}*/
 
-#define BACKLOG 10	 // how many pending connections queue will hold
-#define MAXDATASIZE 100 
+int main(int argc, char *argv[]) {
+	int socket_fd, socket_nuevo_fd, nro_puerto, tam_dir_cliente;
+	char buffer[256];
+	struct sockaddr_in dir_servidor, dir_cliente;
+	int n;
 
-void sigchld_handler(int s)
-{
-	while(waitpid(-1, NULL, WNOHANG) > 0);
-}
-
-int main(void)
-{
-	int fd, nfd;  		// listen en sock_fd. Las aceptadas las abro en nfd
-	struct sockaddr_in myaddr;
-	struct sockaddr_in remoteaddr;
-	socklen_t sin_size;
-	struct sigaction sa;
-	int yes=1;
-	char buf[MAXDATASIZE];
-	int cant;
-
-	if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-		perror("socket");
+	if (argc < 2) {
+		fprintf(stderr,"ERROR, no port provided\n");
 		exit(1);
 	}
 
-	if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-		perror("setsockopt");
-		exit(1);
-	}
-	
-	myaddr.sin_family = AF_INET;
-	myaddr.sin_port = htons(PORT);	 // BigEndian <- Formato de Red.
-	myaddr.sin_addr.s_addr = htonl(INADDR_ANY); // La IP que tenga el servidor.
-	memset(&(myaddr.sin_zero), '\0', 8); // zero the rest of the struct
+	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-	// Hago un bind, para enlazarme a la direccion disponible local.
-	if (bind(fd, (struct sockaddr *)&myaddr, sizeof(struct sockaddr)) == -1) {
-		perror("bind");
-		exit(1);
-	}
+	if (socket_fd < 0) 
+		error("ERROR opening socket");
 
-	// Activo la ecucha de peticiones.
-	if (listen(fd, BACKLOG) == -1) {
-		perror("listen");
-		exit(1);
-	}
+	bzero((char *) &dir_servidor, sizeof(dir_servidor));
+	nro_puerto = atoi(argv[1]);
+	dir_servidor.sin_family = AF_INET;
+	dir_servidor.sin_addr.s_addr = INADDR_ANY;
+	dir_servidor.sin_port = htons(nro_puerto);
 
-	printf("Escuchando en IP: %s Puerto: 2222\n", inet_ntoa(myaddr.sin_addr)); 
+	if (bind(socket_fd, (struct sockaddr *) &dir_servidor,
+			sizeof(dir_servidor)) < 0) 
+		error("ERROR on binding");
 
-	sa.sa_handler = sigchld_handler; // reap all dead processes
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
-	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-		perror("sigaction");
-		exit(1);
-	}
+	listen(socket_fd,5);
+	tam_dir_cliente = sizeof(dir_cliente);
+	socket_nuevo_fd = accept(socket_fd, 
+			(struct sockaddr *) &dir_cliente, 
+			&tam_dir_cliente);
 
-	while(1) {  // Bucle infinito. Se corta con señales.
-		sin_size = sizeof(struct sockaddr_in);
-		// Comienzo a aceptar requests.
-		if ((nfd = accept(fd, (struct sockaddr *)&remoteaddr, &sin_size)) == -1) {
-			perror("accept");
-			continue;
-		}
-		if (!fork()) { // Este es el hijo.
-			close(fd); // child doesn't need the listener
-			if ((cant=recv(nfd, buf, MAXDATASIZE-1, 0)) == -1) {
-				perror("recv");
-				exit(1);
-			}
-		
-			buf[cant] = '\0';
+	if (socket_nuevo_fd < 0) 
+		error("ERROR on accept");
 
-			printf("Request de IP: %s Puerto: %d\n\n", inet_ntoa(remoteaddr.sin_addr),ntohs(remoteaddr.sin_port)); 
-			printf("El cliente dice: %s\n",buf);
-			if (send(nfd, "Pong!\n", 14, 0) == -1)
-				perror("send");
-			close(nfd);
-			exit(0);
-		}
-		close(nfd);  // Cierro el descriptor en el padre del FORK
-	}
+	bzero(buffer,256);
+	n = read(socket_nuevo_fd,buffer,255);
+	if (n < 0) error("ERROR reading from socket");
+	printf("Here is the message: %s\n",buffer);
+	n = write(socket_nuevo_fd,"I got your message",18);
+	if (n < 0) error("ERROR writing to socket");
 
-	return 0;
+	return 0; 
 }
 
